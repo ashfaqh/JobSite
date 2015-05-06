c7884f21051c1efd539267fa1bc0fd2fa5e6495d
ashfaqh <ashfaq.hussain@gmail.com>
2015-05-06 22:25:32 +0200

    Side Nav fixed; Added jquery plugin for form validation.

--- scripts/main.js
+++ scripts/main.js
@@ -2,29 +2,33 @@
  * Created by ashfaq on 04/05/2015.
  */
 
+
 $(function(){
 
+    var recruiterForm = $(".recruiter-form");
+    var candidateForm = $(".candidate-form");
+    var headerNav = $("header > nav");
     var formOpen = false;
 
-    $(".recruiter-form").hide();
-    $(".candidate-form").hide();
+    recruiterForm.hide();
+    candidateForm.hide();
 
-    $("#recruiter_form").on("click", function() {
-        $(".recruiter-form").show('slow');
-        $("header > nav").slideUp("50");
+    $("#recruiter_form_button").on("click", function() {
+        recruiterForm.show('slow');
+        headerNav.slideUp("50");
         formOpen = true;
     });
 
-    $("#candidate_form").on("click", function(){
-        $(".candidate-form").show('slow');
-        $("header > nav").slideUp("50");
+    $("#candidate_form_button").on("click", function(){
+        candidateForm.show('slow');
+        headerNav.slideUp("50");
         formOpen = true;
     });
 
     $(".remove-button").on("click", function(){
-        $(".recruiter-form").hide('slow');
-        $(".candidate-form").hide('slow');
-        $("header > nav").slideDown("50");
+        recruiterForm.hide('slow');
+        candidateForm.hide('slow');
+        headerNav.slideDown("50");
         formOpen = false;
     });
 
@@ -32,10 +36,19 @@ $(function(){
         var scroll = $(window).scrollTop();
         console.log(scroll);
         if (scroll > (2 / 3 * $(window).height())) {
-            $("header > nav").slideUp("200");
+            headerNav.slideUp("200");
         } else if (!formOpen) {
-            $("header > nav").slideDown("200");
+            headerNav.slideDown("200");
         }
     });
 
+    $("#recruiter_form").submit(function(event){
+        event.preventDefault();
+        $(this).validate();
+    });
+
+    $("#candidate_form").submit(function(event){
+        event.preventDefault();
+        $(this).validate();
+    });
 });
